175e0068196620ec8c24ce1988c2892397ad9291
unknown <souravkantigayen@gmail.com>
2020-06-16 21:57:06 +0530

    initial Commit

---
+++ AzureTest/src/main/java/com/azr/boot/AzureRestController.java
@@ -0,0 +1,13 @@
+package com.azr.boot;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AzureRestController {
+
+	@GetMapping("/getResult")
+	public String getResult() {
+		return "I am a service to validate Azure Pipe Line ";
+	}
+}
---
+++ AzureTest/src/test/java/com/azr/boot/AzureTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.azr.boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AzureTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
