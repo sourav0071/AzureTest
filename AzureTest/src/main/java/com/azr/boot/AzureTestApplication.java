4b669c475c47025336832d77385cb4f72ab34e75
unknown <souravkantigayen@gmail.com>
2020-06-17 15:57:32 +0530

    Converting jar to war

--- AzureTest/src/main/java/com/azr/boot/AzureTestApplication.java
+++ AzureTest/src/main/java/com/azr/boot/AzureTestApplication.java
@@ -2,9 +2,10 @@ package com.azr.boot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class AzureTestApplication {
+public class AzureTestApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(AzureTestApplication.class, args);
